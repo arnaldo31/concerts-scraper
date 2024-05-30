c3a3667063d6c03c2ea89d0bd272b2ee38faaa4c
arnaldo31 <kaduray@gmail.com>
2024-05-31 02:53:22 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from scrapers  import pumpehuset,hotelcecil,culturebox,loppen,rust ## this is the 1st barch 5 website 
+from datetime import datetime
+import json
+import codecs
+
+savefile = []
+
+def save():
+    current_date = datetime.now().strftime('%Y%m%d')
+    with codecs.open(f'.\\savefiles\\save_{current_date}.json', 'w', encoding='utf-8') as f:
+        json.dump({'posts': savefile}, f, ensure_ascii=False, indent=4)
+        
+def crawler():
+    
+    data = rust.run()
+    savefile.extend(data)
+    
+    data = hotelcecil.run()
+    savefile.extend(data)
+    
+    data = culturebox.run()
+    savefile.extend(data)
+    
+    data = loppen.run()
+    savefile.extend(data)
+    
+    data = pumpehuset.run()
+    savefile.extend(data)
+
+if __name__ == '__main__':
+    crawler()
+    save()
